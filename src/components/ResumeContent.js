436c61ef3c49082e5943fd1b676a6b82fbe2335b
Quang Tran <tranvietquang3077@gmail.com>
2020-09-28 12:44:27 +0700

    update ava

--- src/components/ResumeContent.js
+++ src/components/ResumeContent.js
@@ -1,46 +1,46 @@
-import React, { useState } from 'react'
-import AboutMe from './AboutMe';
-import Education from './Education';
-import Experience from './Experience';
-import Skills from './Skills';
-import Portfolio from './Portfolio';
-import Interests from './Interests';
+import React, { useState } from "react";
+import AboutMe from "./AboutMe";
+import Education from "./Education";
+import Experience from "./Experience";
+import Skills from "./Skills";
+import Portfolio from "./Portfolio";
+import Interests from "./Interests";
 import { Col, Row } from "react-bootstrap";
-import Carousel from 'react-bootstrap/Carousel'
-
-
+import Carousel from "react-bootstrap/Carousel";
 
 export default function ResumeContent() {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-    return (
-        <Carousel activeIndex={index} onSelect={handleSelect} className="quang-style">
-            <Carousel.Item>
-                <AboutMe />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Education />
-
-
-            </Carousel.Item>
-            <Carousel.Item>
-                <Experience />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Skills />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Portfolio />
-            </Carousel.Item>
-            <Carousel.Item>
-                <Interests />
-            </Carousel.Item>
-        </Carousel>
-    );
+  return (
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      className="quang-style"
+    >
+      <Carousel.Item>
+        <AboutMe />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Education />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Experience />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Skills />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Portfolio />
+      </Carousel.Item>
+      <Carousel.Item>
+        <Interests />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 //     const target = React.createRef();
 //     return (
